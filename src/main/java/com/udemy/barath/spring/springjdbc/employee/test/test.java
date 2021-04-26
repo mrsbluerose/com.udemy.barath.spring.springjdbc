62620b90069036f355ed0b8be69651890bfd4318
Audra Wathen <mrsbluerose@gmail.com>
2021-04-26 16:05:01 -0600

    continues spring jdbc unit

--- src/main/java/com/udemy/barath/spring/springjdbc/employee/test/test.java
+++ src/main/java/com/udemy/barath/spring/springjdbc/employee/test/test.java
@@ -4,14 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.udemy.barath.spring.springjdbc.employee.dao.EmployeeDao;
+
 public class test {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/udemy/barath/spring/springjdbc/config.xml");
-		JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
-		String sql="INSERT INTO employee VALUES(?,?,?)";
-		int result = jdbcTemplate.update(sql,1,"John","Smith");
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/udemy/barath/spring/springjdbc/employee/test/config.xml");
+		EmployeeDao jdbcTemplate = (EmployeeDao) context.getBean("employeeDao");
+		int result = dao.create(new Employee());
 		System.out.println("Number of records inserted: " + result);
 	}
 
